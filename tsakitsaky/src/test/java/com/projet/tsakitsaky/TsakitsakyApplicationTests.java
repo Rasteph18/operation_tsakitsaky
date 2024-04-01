683fd1d08c35aec376357d0292457fa53252078a
Rasteph18 <srandrianarivelo18@gmail.com>
2024-04-01 09:23:09 +0300

    projet spring boot mvc

---
+++ tsakitsaky/src/test/java/com/projet/tsakitsaky/TsakitsakyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.tsakitsaky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsakitsakyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
